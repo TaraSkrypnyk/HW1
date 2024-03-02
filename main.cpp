3965efa5321e43fd39808f4c3541fb709241b5bf
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-03-02 17:28:31 +0200

    Ex4 was done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,24 @@
 #include <iomanip>
 #include <ctime>
 #include <cmath>
+#include <string>
 using namespace std;
 
+int main() {
+    string n, a, v;
+    double k;
+    cout << "Vvedit nazvu knyhy: ";
+    std::getline(std::cin, n);
+    cout << "Vvedit authora knyhy: ";
+    std::getline(std::cin, a);
+    cout << "Vvedit vidannya knyhy: ";
+    std::getline(std::cin, v);
+    cout << "Vvedit kilkist storinok knyhy: ";
+    cin >> k;
+    cout << "Name       : \"" << n << "\"" << endl;
+    cout << "Avtor      : " << a << endl;
+    cout << "Izdatelstvo: " << v << endl;
+    cout << "Pages      : " << k << endl;
 
+    return 0;
+}
