63b571277395abe16e2de966bfb361918b53525c
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-03-02 17:47:07 +0200

    Ex5 was done

--- main.cpp
+++ main.cpp
@@ -4,4 +4,10 @@
 #include <cmath>
 using namespace std;
 
-
+int main() {
+    cout << "\"Kraj lukomor’a dub zelenyj," << endl;
+    cout << "I zolotyj lancyuh na nim :" << endl;
+    cout << "Shodnya, shonochi kit uchenyj" << endl;
+    cout << "Na lancyuhu kruzhl’aje tim\"" << endl;
+    return 0;
+}
