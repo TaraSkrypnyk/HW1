3ddff2a19856b1d9a6877f6fa7c8e2347ba37dc3
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-03-02 17:11:46 +0200

    Ex1 was done

--- main.cpp
+++ main.cpp
@@ -4,4 +4,17 @@
 #include <cmath>
 using namespace std;
 
+int main() {
 
+    double R1, R2, R3, R0;
+    cout << "Vvedit pershiy opir: ";
+    cin >> R1;
+    cout << "Vvedit druhiy opir: ";
+    cin >> R2;
+    cout << "Vvedit tretiy opir: ";
+    cin >> R3;
+    R0 = 1 / ((1 / R1) + (1 / R2) + (1 / R3));
+    cout << "R0 =  " << R0 << endl;
+
+    return 0;
+}
