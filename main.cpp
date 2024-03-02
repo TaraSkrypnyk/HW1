55ddf36092c00a7407cdecff4de274cf82122bb3
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-03-02 17:16:26 +0200

    Ex3 was done

--- main.cpp
+++ main.cpp
@@ -4,4 +4,16 @@
 #include <cmath>
 using namespace std;
 
+int main() {
+    double S, v, t, a;
+    cout << "Vvedit shvidkist ruhu: ";
+    cin >> v;
+    cout << "Vvedit chas ruhu: ";
+    cin >> t;
+    cout << "Vvedit priskorenna ruhu: ";
+    cin >> a;
+    S = v * t + (a * t * t) / 2;
+    cout << "S =  " << S << endl;
 
+    return 0;
+}
