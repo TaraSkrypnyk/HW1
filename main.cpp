ee3cdb105b722779e10797830158470aea13d912
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-03-02 17:13:28 +0200

    Ex2 was done

--- main.cpp
+++ main.cpp
@@ -4,4 +4,14 @@
 #include <cmath>
 using namespace std;
 
+int main() {
+    const double pi = 3.14;
+    double L, R, S;
+    cout << "Vvedit dovzhynu kola: ";
+    cin >> L;
+    R = L / (2 * pi);
+    S = 2 * pi * R * R;
+    cout << "S =  " << S << endl;
 
+    return 0;
+}
